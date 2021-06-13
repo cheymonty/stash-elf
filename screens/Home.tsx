071e17688c4e9df0b9539fdff3f2a2b439a3535b
Cheyenne Montgomery <chey.monty@gmail.com>
2021-06-13 11:19:59 -0400

    added name dialog

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { Appbar } from 'react-native-paper';
+import { Appbar, Title, Dialog, Portal, Button, TextInput } from 'react-native-paper';
 
 export default function Home() {
+  const [firstBoot, setFirstBoot] = useState(true);
+  const [name, setName] = useState("");
+
+
   return (
     <View style={styles.container}>
-        <Appbar.Header>
-            <Appbar.Content title="Stash-Elf"/>
-        </Appbar.Header>
-        <Text>HomeScreen</Text>
+      <Appbar.Header>
+        <Appbar.Content title="Stash Elf"/>
+      </Appbar.Header>
+
+      <Portal>
+        <Dialog visible={firstBoot} dismissable={false}>
+          <Dialog.Title>Hi! I'm Your Helper Elf. What Would You Like Me to Call You?</Dialog.Title>
+
+          <Dialog.Content>
+            <TextInput label="Name" value={name} onChangeText={text => setName(text)}/>
+          </Dialog.Content>
+
+          <Dialog.Actions>
+            <Button disabled={name.length === 0 ? true : false} onPress={() => setFirstBoot(false)}>Done</Button>
+          </Dialog.Actions>
+        </Dialog>
+      </Portal>
+
+
+      <Title>{name}</Title>
     </View>
   );
 }
